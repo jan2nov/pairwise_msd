28028971561cd10c941b3427dd63fe2c6dd3b509
jan2nov <jan.novotny@physics.slu.cz>
2020-10-14 09:12:00 +0000

    pull request

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+
+
+
+ // end of the code
+ fflush(stdout); 
+ printf("\n\t**********************************\n");
+ printf( "\t        #That's all folks!        \n");
+ printf( "\t**********************************\n");
+
+ return 0;
+}
